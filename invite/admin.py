85a8adc7486e8a75e41148bfab3674971989304a
lmarvaud <24732919+lmarvaud@users.noreply.github.com>
2018-10-31 16:23:19 +0100

    feat: display families in the admin
    Signed-off-by: lmarvaud <24732919+lmarvaud@users.noreply.github.com>

--- invite/admin.py
+++ invite/admin.py
@@ -1,3 +1,27 @@
 """
 Admin configurations for django-invite project
 """
+from django.contrib import admin
+from .models import Family, Invite, Accompany
+
+
+class InviteInline(admin.TabularInline):
+    """
+    Family guest admin view
+    """
+    model = Invite
+
+
+class AccompanyInline(admin.TabularInline):
+    """
+    Family accompanies admin view
+    """
+    model = Accompany
+
+
+@admin.register(Family, site=admin.site)
+class FamilyAdmin(admin.ModelAdmin):
+    """
+    Family admin view
+    """
+    inlines = [InviteInline, AccompanyInline]
