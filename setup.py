80e41e97887b687ec3373b98211b7769d15ebb48
lmarvaud <24732919+lmarvaud@users.noreply.github.com>
2019-04-21 01:58:06 +0200

    ci: use tox to test with multiple python and django versions

--- setup.py
+++ setup.py
@@ -16,14 +16,20 @@ setup(
         'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
         'Environment :: Web Environment',
+        'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: French',
         'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Communications :: Email',
         'Topic :: Communications :: Email :: Address Book',
         'Topic :: Communications :: Email :: Mailing List Servers',
@@ -34,13 +40,9 @@ setup(
     packages=find_packages(exclude=['demo']),
     include_package_data=True,
     install_requires=[
-        'django>2.1.5,<=2.1.7',
-        'Pillow==6.0.0',
-        'pytz==2018.7'
+        'django>=2.0'
     ],
     dependency_links=[
-        'django>2.1.5,<=2.1.7',
-        'Pillow==6.0.0',
-        'pytz==2018.7'
+        'django>=2.0'
     ],
 )
